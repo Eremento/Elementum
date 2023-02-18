058537497b02cae523dd271289530d496e436b6c
DaRK9 <107148377+Eremento@users.noreply.github.com>
2023-02-18 17:03:56 +0300

    functions

--- main_v1/main_v1.ino
+++ main_v1/main_v1.ino
@@ -68,25 +68,11 @@ void notrun() {
 }
 
 void turn_right() {
-//  digitalWrite(RIGHT_MOTOR,HIGH);
-//  digitalWrite(RIGHT_MOTOR_PWM,HIGH);    
-//  analogWrite(RIGHT_MOTOR_PWM,90);
-//  
-//  digitalWrite(LEFT_MOTOR,LOW);
-//  digitalWrite(LEFT_MOTOR_PWM,HIGH);
-//  analogWrite(LEFT_MOTOR_PWM,SPEED);
 analogWrite(LEFT_MOTOR_PWM,SPEED);
 analogWrite(RIGHT_MOTOR_PWM,0);
 }
 
 void turn_left() {
-//  digitalWrite(RIGHT_MOTOR,LOW);
-//  digitalWrite(RIGHT_MOTOR_PWM,HIGH);    
-//  analogWrite(RIGHT_MOTOR_PWM,SPEED);
-//  
-//  digitalWrite(LEFT_MOTOR,HIGH);
-//  digitalWrite(LEFT_MOTOR_PWM,HIGH);
-//  analogWrite(LEFT_MOTOR_PWM,90);
 analogWrite(LEFT_MOTOR_PWM,0);
 analogWrite(RIGHT_MOTOR_PWM,SPEED);
 }
@@ -105,6 +91,20 @@ void pick_up() {
    arrow.write(0);
 }
 
+void down() {
+  arow.write(0);
+  delay(800);
+  air.write(180);
+  delay(800);
+  claw.write(10);
+  delay(800);
+  arrow.write(120);
+  delay(800);
+  air.write(120);
+  delay(800);
+  claw.write(90);
+}
+
 void loop() {
   IS_BLACK_RIGHT = digitalRead(LINE_SENSOR_RIGHT);
   IS_BLACK_MID = digitalRead(LINE_SENSOR_MID);
@@ -120,7 +120,6 @@ void loop() {
   duration = pulseIn(ECHO, HIGH);
   
   distance= duration*0.034/2;
-//  Serial.print("distation ");
 //  Serial.println(distance);
 
 
@@ -136,9 +135,25 @@ void loop() {
     }
     Serial.println(QR);
   }
-  if (QR.indexOf("c") == 0) {
+  if (QR.indexOf("b") == 0) {
     pick_up();
-    Serial.println("test");
+    Serial.println("up");
+  }
+  }
+
+    if (distance == 20){
+    if (QR_Scanner.available()) 
+  {
+    while (QR_Scanner.available()) 
+    {
+      char i = QR_Scanner.read(); 
+      QR += i; 
+    }
+    Serial.println(QR);
+  }
+  if (QR.indexOf("p") == 0) {
+    down();
+    Serial.println("down");
   }
   }
   
